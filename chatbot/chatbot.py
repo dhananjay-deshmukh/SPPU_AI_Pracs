d8a506616357dfe708eb3d2ca033fd7fde258de5
Dhananjay Deshmukh <112422798+dhananjay-deshmukh@users.noreply.github.com>
2023-05-25 17:10:12 +0530

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,78 +1,55 @@
-import streamlit as st
+import  streamlit as st
 import json
 
-# Define the restaurant chatbot
-chatbot_rules = {
-        "hi": "Hello! How can I assist you today?",
-        "menu": "Sure! Here's our menu: idli, vada, sambar",
-        "reservation": "To make a reservation, please call us or email us.",
-        "location": "We are located at [insert address].",
-        "thank you": "You're welcome! Enjoy your meal!",
-        "default": "I'm sorry, but I'm not able to assist with that. How else can I help you?",
-        "phone" : "Its 7689054368",
-        "email": "restaurant@gmail.com"
-    }
-
 def load_dictionary():
-    try:
-        with open("dictionary.json", "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return {}
+    with open('dictionary.json', 'r') as file:
+        return json.load(file)
+        
 
-# Function to save the dictionary to a file
 def save_dictionary(dictionary):
-    with open("dictionary.json", "w") as file:
+    with open('dictionary.json', 'w') as file:
         json.dump(dictionary, file)
 
-def restaurant_chatbot(user_input):
+chatbot_rules = load_dictionary()
+
+def chatbot(user_input):
     chatbot_rules = load_dictionary()
     user_input = user_input.lower()
-    
-    for key in chatbot_rules:
-        if key in user_input:
-            return chatbot_rules[key]
-    
-    return chatbot_rules["default"]
 
+    for key in chatbot_rules:
+        if key in user_input :
+            return chatbot_rules[key]
+
+    return chatbot_rules['default']
 
 def main():
-    nav = st.sidebar.radio("Navigate", ["Chat", "Add Questions"])
-    quit = st.sidebar.button("Quit")
-
-    if quit:
+    st.header("Welcome to Restobot!")
+    nav = st.sidebar.radio('Navigation', ['Chat', 'Add Questions'])
+    if st.sidebar.button('Exit'):
         st.empty()
-        st.header("Thank you for using Restobot!")
+        st.title("Thanks for using RestoBot")
         st.stop()
-        
 
-    if nav == "Chat":
-        
-        st.title("Restaurant Chatbot")
-        st.markdown("Welcome to our restaurant chatbot!")
+    if nav == 'Chat':
+        user_input = st.text_input("Ask restobot")
+        if st.button("Submit"):
+            bot_response = chatbot(user_input=user_input)
+            st.write("Restobot:", bot_response)
+            st.text("______________________________________")
 
-        user_input = st.text_input("User Input")
-        submitted = st.button("Submit")
+    if nav == 'Add Questions':
+        add_question = st.text_input("Add your keyword here")
+        add_answer = st.text_input("Add the corrosponding response")
+        if add_question and add_answer:
+            if st.button("add question"):
+                chatbot_rules[add_question] = add_answer
+                save_dictionary(chatbot_rules)
+                st.success("Added")
 
-        if submitted:
 
-            bot_response = restaurant_chatbot(user_input)
-            st.text("Bot: " + bot_response)
-            st.text("___________________________")  # Separator line
-
-            
-    elif nav == "Add Questions":
-        add_question = st.text_input("Add keyword here", key = 'key')
-        add_answer = st.text_input("Provide a response here", key='value')
-
-        if st.button("Add rule"):
-            chatbot_rules[add_question] = add_answer
-            save_dictionary(chatbot_rules)
-            st.success("Question and answer added successfully!")
-
-        st.write("Existing Dictionary:", chatbot_rules)
-            
                 
 if __name__ == '__main__':
     main()
+
+
 
